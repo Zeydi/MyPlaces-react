a3c2cf5d5e4b4eea4e684c7554945846639d1298
Zeydi <oussema.zeidi@ieee.org>
2020-08-16 09:06:12 +0200

    added folders structue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+const App = () => {
+  return <Router></Router>;
+};
 
 export default App;
