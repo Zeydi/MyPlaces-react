1ca0ba1d13b2ac29ff39a4c489a23f6480b38318
Zeydi <oussema.zeidi@ieee.org>
2020-08-17 12:35:38 +0200

    added users components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h1>USers works</h1>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'wess',
+      image:
+        'https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg',
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
