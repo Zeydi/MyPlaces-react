3b30468b6ac36c17f57f01867a90a5224d7fa4dc
Zeydi <oussema.zeidi@ieee.org>
2020-08-17 12:59:23 +0200

    fixed UserItem style

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -8,22 +8,19 @@ import './UserItem.css';
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item_content">
-        <Card>
-          <Link to={`/${props.id}/places`}>
-            <div className="user-item_image">
-              <Avatar image={props.image} alt={props.name} />
-            </div>
-            <div className="user-item_info">
-              <h2>{props.name}</h2>
-              <h3>
-                {props.placeCount} :{' '}
-                {props.placeCount === 1 ? 'Place' : 'Places'}
-              </h3>
-            </div>
-          </Link>
-        </Card>
-      </div>
+      <Card className="user-item_content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item_image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item_info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} : {props.placeCount === 1 ? 'Place' : 'Places'}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
