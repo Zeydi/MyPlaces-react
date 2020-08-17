501aa8a3b4cb9b55d43649ee40f69e75ec4de3f3
Zeydi <oussema.zeidi@ieee.org>
2020-08-17 12:58:04 +0200

    added Card component and fixed UserItem style

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Card from '../../shraed/UIElements/Card';
 import Avatar from '../../shraed/UIElements/Avatar';
 import './UserItem.css';
 
@@ -8,17 +9,20 @@ const UserItem = (props) => {
   return (
     <li className="user-item">
       <div className="user-item_content">
-        <Link to={`/${props.id}/places`}>
-          <div className="user-item_image">
-            <Avatar image={props.image} alt={props.name} />
-          </div>
-          <div className="user-item_info">
-            <h2>{props.name}</h2>
-            <h3>
-              {props.placeCount} : {props.placeCount === 1 ? 'Place' : 'Places'}
-            </h3>
-          </div>
-        </Link>
+        <Card>
+          <Link to={`/${props.id}/places`}>
+            <div className="user-item_image">
+              <Avatar image={props.image} alt={props.name} />
+            </div>
+            <div className="user-item_info">
+              <h2>{props.name}</h2>
+              <h3>
+                {props.placeCount} :{' '}
+                {props.placeCount === 1 ? 'Place' : 'Places'}
+              </h3>
+            </div>
+          </Link>
+        </Card>
       </div>
     </li>
   );
