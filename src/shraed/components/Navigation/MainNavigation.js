bbc49c83420bc696d3ee599def3da3804fab1773
Zeydi <oussema.zeidi@ieee.org>
2020-08-18 12:33:26 +0200

    fixed navbar style

--- src/shraed/components/Navigation/MainNavigation.js
+++ src/shraed/components/Navigation/MainNavigation.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import SideDrawer from './SideDrawer';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import './MainNavigation.css';
 
 const MainNavigation = (props) => {
   return (
-    <MainHeader>
-      <button className="main-navigation_menu-btn"></button>
-      <h2 className="main-navigation_title">
-        <Link to="/">YourPlaces</Link>
-      </h2>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation_drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation_menu-btn"></button>
+        <h2 className="main-navigation_title">
+          <Link to="/">YourPlaces</Link>
+        </h2>
+        <nav className="main-navigation_header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
