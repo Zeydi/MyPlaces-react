f09a335cfe16821da41f41a1cd4fc458521f175c
Zeydi <oussema.zeidi@ieee.org>
2020-08-19 19:13:03 +0200

    fixed header components style

--- src/shraed/components/Navigation/MainNavigation.js
+++ src/shraed/components/Navigation/MainNavigation.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import Backdrop from '../UIElements/Backdrop';
 import SideDrawer from './SideDrawer';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import './MainNavigation.css';
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <React.Fragment>
-      <SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation_drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
+      )
       <MainHeader>
-        <button className="main-navigation_menu-btn"></button>
+        <button
+          className="main-navigation_menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span>...</span>
+        </button>
         <h2 className="main-navigation_title">
           <Link to="/">YourPlaces</Link>
         </h2>
