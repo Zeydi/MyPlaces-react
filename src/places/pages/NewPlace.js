0c24beadc082de0b184d756d118a511761faa2f0
Zeydi <oussema.zeidi@ieee.org>
2020-08-24 13:24:22 +0200

    added form component for newPlace

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Input from '../../shraed/components/FormElements/Input';
+import './NewPlace.css';
+
 const NewPlace = () => {
-  return <div>NewPlaces</div>;
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title"></Input>
+    </form>
+  );
 };
 
 export default NewPlace;
